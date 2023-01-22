bcefd6ae91f589b0a632c35ea23e7756d06e5ca9
B-ZONE <oreshkin111@gmail.com>
2023-01-22 15:43:17 +0300

    added new files index.html and script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+// console.log('Текст из script.js');
+// let num = 123;
+// console.log(num);
+// let a = 5;
+// a = 7;
+// console.log(a);//выводим значение переменной а
+// let b = 1 + 2 + 3;
+// console.log(b);
+/*Задание 2 завершено
+успешно*/
+
+// Задание 3
+
+// let c = 10;
+// let d = 2;
+// let sum = c + d;
+// console.log(sum);
+// let resultSubtraction
+// console.log(resultSubtraction);
+// let resultMultiplication = c * d;
+// console.log(resultMultiplication);
+// let resultDivision = c / d;
+// console.log(resultDivision);
+// let sumAllVariables = a + b + c + d + num + resultMultiplication + resultDivision;
+// console.log(sumAllVariables); 
+// Задание 3 успешно завершено
+
+// Задание 4
+
+// Не запуская код, определите, что выведется в консоль: 
+// let a = 5 + 5 * 3 + 3; a = 23;
+// Не запуская код, определите, что выведется в консоль:
+// let a = 8 / 2 + 2; a = 6;
+// Не запуская код, определите, что выведется в консоль:
+// let a = (2 + 3) * (2 + 3); a= 25;
+// Не запуская код, определите, что выведется в консоль:
+// let a = (2 + 3) * 2 + 3; a = 13;
+// Не запуская код, определите, что выведется в консоль:
+// let a = (2 * 8) / 4; a = 4;
+// Не запуская код, определите, что выведется в консоль:
+// let a = 2 * (8 / 4); a = 4;
+// Задание 4 выполнено завершено
+
+// Задание 5
+
+// let  a = 1.5;
+// let b = 0.75;
+// let sum = a + b;
+// console.log(sum);
+// let a = 50;
+// console.log(-a);
+// Задание 5 завершено
+
+// Задание 6
+
+// const userName = `Sergei`;
+// const userLastName = `Oreshkin`;
+// let empty = ' ';
+// console.log(userName + empty + userLastName);
+// let first = 'Java';
+// let second = 'Script';
+// console.log(first + second);
+// let hello = 'Hello';
+// let world = 'World';
+// let empty = ' ';
+// console.log(hello + empty + world); 
+
+// Дополнительное задание
+
+// let lengthParallepiped = 3;
+// let widthParallepiped = 5;
+// let heightParallepiped = 10;
+// let volumeParallepiped = lengthParallepiped * widthParallepiped * heightParallepiped;
+// console.log(volumeParallepiped);
